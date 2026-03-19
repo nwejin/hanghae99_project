47ecafdb3899ecc983d334d85944c84878466f06
nwejin <147528674+nwejin@users.noreply.github.com>
2026-03-19 20:50:17 +0900

    update ui

--- components/common/header/header.tsx
+++ components/common/header/header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
   const { nickname, profileImg, isLoggedIn } = useCurrentUser();
 
   return (
-    <header className="flex h-[60px] shrink-0 items-center justify-between border-b border-paw-border bg-white px-4">
+    <header className="flex h-[60px] shrink-0 items-center justify-between border-b-2 border-paw-cream bg-paw-cream-dark px-4">
       <Link href="/">
         <Image src={text_logo} alt="멍냥터 로고" width={120} />
       </Link>
@@ -17,7 +17,7 @@ export default function Header() {
       {isLoggedIn ? (
         <Link
           href={`/user/${nickname}`}
-          className="h-8 w-8 overflow-hidden rounded-full border-2 border-paw-border bg-paw-cream-dark">
+          className="h-8 w-8 overflow-hidden rounded-full border-2 border-paw-tag bg-paw-main">
           {profileImg ? (
             <img src={profileImg} alt="프로필" className="h-full w-full object-cover" />
           ) : (
